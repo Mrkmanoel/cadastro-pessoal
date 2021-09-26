a8ec7338203cd4ac3a5fc2481236746be7dc27c4
Manoel Venceslau Dias FIlho <mw.filho43@gmail.com>
2021-09-26 15:00:08 -0300

    Atualizando os comentários em JavaScript

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,7 +1,11 @@
+// Este é apenas um teste de resultado no console do navegaodr
 console.log("Ola Manoel");
 
+// Uma função capturada pelo evento em JavaScript num formulário
 function enviarSubmeter()
 {
+    // Um alerta aparecerá no navegador uma vez que submeter o formulário
     alert("Formulário enviado com sucesso!");
+    // Encmainhamento da próxima página
     window.location.href = "sucesso.html";
 }
